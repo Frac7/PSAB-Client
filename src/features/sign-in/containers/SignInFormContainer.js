c5b01b0e1f94d42e95ba62edfa102ea9778dab94
Francesca Cella <francescacella@easy-network.it>
2020-08-16 23:21:26 +0200

    Cognito authentication WIP

--- src/features/sign-in/containers/SignInFormContainer.js
+++ src/features/sign-in/containers/SignInFormContainer.js
@@ -1,12 +1,22 @@
 import React, { useMemo, useCallback } from 'react';
 
+import {
+    AuthenticationDetails,
+    CognitoUserPool,
+    CognitoUser
+  } from "amazon-cognito-identity-js";
 import { string, object } from 'yup';
 import { Formik } from 'formik';
 import { Row, Col } from 'reactstrap';
 
 import SignInForm from '../components';
 
-const SignIn = () => {
+const SignIn = ({ history }) => {
+    const userPool = useMemo(() => new CognitoUserPool({
+        UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+        ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID
+    }), []);
+
     const initialValues = useMemo(() => ({ 
         email: '', 
         password: '' 
@@ -16,10 +26,27 @@ const SignIn = () => {
             email: string().email('E-mail non valida').required('Inserire l\'e-mail'), 
             password: string().required('Inserire la password') 
         })), []);
-    const onSubmit = useCallback((values, { setSubmitting, setErrors }) => {
-        console.log(values);
-        setTimeout(() => setSubmitting(false), 1500);
-    }, []);
+    const onSubmit = useCallback(({ email, password }, { setSubmitting, setErrors }) => {
+        const authDetails = new AuthenticationDetails({ Username: email, Password: password });
+        const cognitoUser = new CognitoUser({ Username: email, Pool: userPool });
+
+        cognitoUser.authenticateUser(authDetails, {
+            onSuccess(result) {
+                console.log(result);
+                window.localStorage.setItem('LoggedUser', email);
+                setSubmitting(false);
+                history.push('/profile');
+
+            },
+            onFailure(error) {
+                console.error(error);
+                setSubmitting(false);
+                setErrors({ password: error.message });
+            }
+        })
+
+        window.localStorage.setItem('UserAuth', JSON.stringify({ email }));
+    }, [history, userPool]);
 
     return (
         <Row>
