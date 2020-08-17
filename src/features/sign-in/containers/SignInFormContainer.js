affc61bfc47def298fde36ce7f85b0de176f5b3d
Francesca Cella <francescacella@easy-network.it>
2020-08-17 11:43:32 +0200

    Sign in behaviour for logged in user improved

--- src/features/sign-in/containers/SignInFormContainer.js
+++ src/features/sign-in/containers/SignInFormContainer.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import {
     AuthenticationDetails,
@@ -10,8 +10,17 @@ import { Row, Col } from 'reactstrap';
 import SignInForm from '../components';
 
 import { userPool, initialValues, validationSchema } from '../values';
+import { PROFILE } from '../../../config/routes';
 
 const SignIn = ({ history }) => {
+    useEffect(() => {
+        const auth = window.localStorage.getItem('LoggedIn');
+
+        if (auth) {
+            history.push(PROFILE);
+        }
+    }, [history]);
+
     const onSubmit = useCallback(({ email, password }, { setSubmitting, setErrors }) => {
         const authDetails = new AuthenticationDetails({ Username: email, Password: password });
         const cognitoUser = new CognitoUser({ Username: email, Pool: userPool });
@@ -21,7 +30,7 @@ const SignIn = ({ history }) => {
                 console.log(result);
                 window.localStorage.setItem('LoggedIn', JSON.stringify(result));
                 setSubmitting(false);
-                history.push('/profile');
+                history.push(PROFILE);
 
             },
             onFailure(error) {
