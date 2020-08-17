e18302dad57fec00e86262bdf898823dfb1a6733
Francesca Cella <francescacella@easy-network.it>
2020-08-17 11:34:44 +0200

    Sign out feature done

--- src/shared/auth/index.js
+++ src/shared/auth/index.js
@@ -14,7 +14,6 @@ const withAuthentication = (AuthComponent) => {
         }
 
         render() {
-            console.log(this.state);
             if (this.state.auth) {
                 return <AuthComponent />;
             }
