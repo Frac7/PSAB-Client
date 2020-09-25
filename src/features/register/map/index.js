af6d96294668176db27b6a4b40c9b850d246e11c
Frac7 <francescacella96@gmail.com>
2020-09-25 14:31:50 +0200

    Certification done

--- src/features/register/map/index.js
+++ src/features/register/map/index.js
@@ -14,36 +14,7 @@ import {
 	MAINTENANCE_ACTIVITIES
 } from '../../../shared/values';
 
-import {
-	Land,
-	Portion,
-	Product,
-	Maintenance,
-	ProductionActivity
-} from '../../../contracts';
-
-const contracts = {
-	[LAND]: {
-		ABI: Land,
-		address: '0xbc35822023d94eB1A47F6EE701cf2028366F64a5'
-	},
-	[PORTION]: {
-		ABI: Portion,
-		address: '0x588Fb29dfE475aC957B90e2ee565f31498a1Fa8b'
-	},
-	[PRODUCT]: {
-		ABI: Product,
-		address: '0xbc35822023d94eB1A47F6EE701cf2028366F64a5'
-	},
-	[MAINTENANCE_ACTIVITIES]: {
-		ABI: Maintenance,
-		address: '0x15e80c90047a6e857B33D321b0b5A826d44Eb97B'
-	},
-	[PROD_ACTIVITIES]: {
-		ABI: ProductionActivity,
-		address: '0x341C54D1626698241354E184EE67ED44a8B1d721'
-	}
-};
+import contracts from '../../../shared/contracts';
 
 const forms = {
 	[LAND]: {
