9af8acabd3d00aa12e355930dd0752f87ec7d695
Frac7 <francescacella96@gmail.com>
2020-09-26 04:29:56 +0200

    Redux Saga configuration

--- src/store/config/configureStore.js
+++ src/store/config/configureStore.js
@@ -1,10 +1,22 @@
-import { compose, createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
+import rootSaga from './sagas';
 
 const configureStore = () => {
+	const middlewares = [];
+	const sagaMiddleware = createSagaMiddleware();
+
+	middlewares.push(sagaMiddleware);
+
 	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-	return createStore(rootReducer, composeEnhancers());
+	const enhancers = composeEnhancers(applyMiddleware(...middlewares));
+
+	const store = createStore(rootReducer, enhancers);
+	sagaMiddleware.run(rootSaga);
+
+	return store;
 };
 
 export default configureStore;
