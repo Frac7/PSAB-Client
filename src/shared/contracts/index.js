6ea0142469992472114d2f2dab332815c4e8ee02
Frac7 <francescacella96@gmail.com>
2020-09-26 04:24:24 +0200

    Contrats updated

--- src/shared/contracts/index.js
+++ src/shared/contracts/index.js
@@ -17,23 +17,23 @@ import {
 const contracts = {
 	[LAND]: {
 		ABI: Land,
-		address: '0xa79f4F2617abCCb4dbEAbe99B61401Fa49f0A8Fd'
+		address: '0xC6595b7EDBc26fAEC6B8557Fc3bbEc07795fFad7'
 	},
 	[PORTION]: {
 		ABI: Portion,
-		address: '0x353A1A99BE50615dd1015caE585df143c0857FD0'
+		address: '0xfA0C6D72F5219411C352F38cDF835e26777F9eB3'
 	},
 	[PRODUCT]: {
 		ABI: Product,
-		address: '0x2aE3f4CF8b0d0e65c7c4356Bbf8fafe68951a421'
+		address: '0xC32676c4E4Ab7E8AB32975562eC5C93FdEFB911A'
 	},
 	[MAINTENANCE_ACTIVITIES]: {
 		ABI: Maintenance,
-		address: '0x25FCaaE613A07CaA9a023644bf26458AC84D34A5'
+		address: '0xF29fDC9C0102CD1658Bd5c8259378fea9447CD17'
 	},
 	[PROD_ACTIVITIES]: {
 		ABI: ProductionActivity,
-		address: '0xb53765118b3A3763cc6B77cB22f870b4F8e03D18'
+		address: '0xf97Ec75A266e9A0b68ec57D66641Ed8afF6da0D5'
 	}
 };
 
