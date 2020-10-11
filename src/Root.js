bf99fc078bfff19d20509fb9c76b6d912ebb6561
Frac7 <francescacella96@gmail.com>
2020-10-11 20:06:23 +0200

    Infura integration WIP

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter } from 'react-router-dom';
+import { Col, Container, Row } from 'reactstrap';
+
+import { useWeb3Network, useEphemeralKey } from '@openzeppelin/network/react';
+import { GSNProvider } from '@openzeppelin/gsn-provider';
+import Wallet from 'ethereumjs-wallet';
+import Web3 from 'web3';
 
 import App from './App';
 import { Header } from './shared/header';
+import { StyledSpinner } from './shared/styled';
 
 const Root = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const { connected } = useWeb3Network('wss://goerli.infura.io/ws/v3/2825ef3aeb9047b7ab6e108500f89b60', {
+		gsn: { signKey: useEphemeralKey() }
+	});
+	useEffect(() => {
+		if (connected) {
+			setIsLoading(false);
+		}
+	}, [connected]);
+
+	window.web3 = new Web3(new GSNProvider('https://goerli.infura.io/v3/2825ef3aeb9047b7ab6e108500f89b60', {
+		signKey: Wallet.generate().privKey
+	}));
+
+	if (isLoading) {
+		return (
+			<Container fluid>
+				<Row className="my-3 justify-content-center align-content-center align-items-center">
+					<Col sm="auto" xl="auto">
+						<h1>Connessione...</h1>
+					</Col>
+				</Row>
+				<Row className="my-3 justify-content-center align-content-center align-items-center">
+					<Col xl="auto" sm="auto">
+						<StyledSpinner size="large"/>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+
 	return (
 		<HashRouter>
 			<Header />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// eslint-disable-next-line
-import Web3 from 'web3';
-
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -18,19 +15,11 @@ import configureAwsAmplify from './config/aws-amplify';
 window.addEventListener('load', () => {
 	configureAwsAmplify();
 
-	if (typeof window.web3 !== 'undefined') {
-		window.web3 = new Web3(window.web3.currentProvider);
-	} else {
-		window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-	}
-
-    window.ethereum.enable(
-		ReactDOM.render(
-			<Provider store={store}>
-				<Root />
-			</Provider>,
-			document.getElementById('root')
-		)
+	ReactDOM.render(
+		<Provider store={store}>
+			<Root />
+		</Provider>,
+		document.getElementById('root')
 	);
 });
 
